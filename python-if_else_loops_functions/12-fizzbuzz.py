3807086743975ab64b4eb7cfc8f57e2094660297
NinaMwangi <n.mwangi@alustudent.com>
2023-02-09 06:58:13 -0800

    Task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 for fizzbuzz in range(0, 101):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 ==0:
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("fizzbuzz")
         continue
     elif fizzbuzz % 3 == 0:
         print("fizz")
         continue
-    else fizzbuzz % 5 == 0:
+    elif fizzbuzz % 5 == 0:
         print("buzz")
         continue
     else:
