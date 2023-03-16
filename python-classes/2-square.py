5c2926ef058daf83de05ebc06c36b424d342f39d
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-17 00:14:05 +0300

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+'''Define the class Square'''
+
+
+class Square:
+    '''Class with size attribute'''
+    def __init__(self, size=0):
+        '''Initialise Square'''
+        if type(size) is not int:
+            raise TypeError('size must be an integer.')
+        if size < 0:
+            raise ValueError('size must be >= 0') 
+        self.__size = size
