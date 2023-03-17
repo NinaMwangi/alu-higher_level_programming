1b737bc5df37c3d51a0e3521baaea870f72d9caa
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-17 11:51:49 +0300

    Task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+'''Define a class Rectangle'''
+
+
+class Rectangle:
+    '''Instantiating the class'''
+
+    def __init__(self, width=0, height=0):
+        '''Constructor'''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''Get the width'''
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        '''Set the width'''
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Get the height'''
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        '''Set the height'''
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    def area(self):
+        '''return the area of the rectangle'''
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        '''Calculating the perimeter'''
+        if self.__height || self.__width == 0
+            return 0
+
+        return (2(self.__height + self.__width))
