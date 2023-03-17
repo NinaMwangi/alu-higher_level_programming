ada64c12a6b75113c1391f7ece66b846b55efab7
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-17 16:48:20 +0300

    Task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -66,10 +66,11 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        '''comparing two rectangles'''
+        '''Compare two rectangles'''
         if type(rect_1) is not Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
-        if type(rect_2) is not a Rectangle:
+
+        if type(rect_2) is not Rectangle:
             raise TypeError('rect_2 must be an instance of Rectangle')
 
         a_rect1, a_rect2 = rect_1.area(), rect_2.area()
