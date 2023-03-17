e1fe8954dcda7d1ebcafb7b5c0ecc0d7fbc2f69b
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-17 16:24:19 +0300

    Task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''Constructor'''
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     def __del__(self):
