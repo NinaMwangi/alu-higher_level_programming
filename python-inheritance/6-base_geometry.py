46839841d6581abd4c364d954bb7452911b5bdf1
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-22 09:07:06 +0300

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''class BaseGeometry'''
+    def area(self):
+        '''method not implemented'''
+        raise Exception('area() is not implemented')
