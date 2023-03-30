95254e1904eb536a78a9a0ccae12e5a1b35e0666
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-30 22:24:44 +0300

    Task 102

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- Displays the top 3 of cities temps during July and Aug ordered by temp (desc)
+SELECT city, AVG(value) AS avg_temp FROM temperatures 
+WHERE month = 7 OR month = 8 
+GROUP BY city 
+ORDER BY avg_temp DESC 
+LIMIT 3;
