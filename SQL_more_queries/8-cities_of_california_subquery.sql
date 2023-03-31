65df40be7aa199c995deeeda2359795548fde935
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-31 20:51:48 +0300

    Task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Script lists all the cities in California found in the database.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
