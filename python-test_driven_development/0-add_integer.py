66570434b83eb976b2fe5fc93518eb79721d5dc9
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-04-15 12:49:23 +0300

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,10 @@
 
 def add_integer(a, b=98):
     '''A function that adds two integers'''
-    if a != int and a != float:
+    type_a, type_b = type(a), type(b)
+    if type_a != int and type_a != float:
         raise TypeError("a must be an integer")
-    if b != int and b != float:
+    if type_b != int and type_b != float:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
