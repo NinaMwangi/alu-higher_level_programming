224970a19e0b070029d6afaa6c0b2722796e6c09
NinaMwangi <n.mwangi@alustudent.com>
2023-06-13 22:37:35 +0300

    Task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.heigh; i++) {
+    for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
