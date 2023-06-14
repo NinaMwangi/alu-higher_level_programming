60ac1c2115f17fb142307ed2a85bd40bd9ea1129
NinaMwangi <n.mwangi@alustudent.com>
2023-06-14 09:11:32 +0300

    Task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const Squared = requirel('./5-square');
 
-module.exports = class Square extends Squared {
+module.exports = class Square extends requirel('./5-square') {
   charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
