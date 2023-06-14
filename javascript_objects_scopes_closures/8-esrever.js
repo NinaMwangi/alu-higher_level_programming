81c98ad5e5999f4538564af6b62f53f06b13dc7f
NinaMwangi <n.mwangi@alustudent.com>
2023-06-14 08:57:28 +0300

    Task 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 
 exports.esrever = function (list) {
   const result = [];
-  for (let i = list.length -1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     result.push(list[i]);
   }
   return result;
-}
+};
