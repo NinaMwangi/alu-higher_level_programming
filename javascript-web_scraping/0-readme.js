4fab12e6d25c2a6ff78752a2ad7ac7508e6aa5a4
NinaMwangi <n.mwangi@alustudent.com>
2023-06-14 22:47:46 +0300

    Task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2],'utf8', function (error, data) {
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
   if (error) {
     console.error(error);
     return;
