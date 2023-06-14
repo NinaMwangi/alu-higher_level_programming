581217c1281bed28876d7cd0d4e2fac835a0020a
NinaMwangi <n.mwangi@alustudent.com>
2023-06-14 09:05:50 +0300

    Task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-export default class Square extends requirrel('./5-square') {
-  charPrint(c) {
+export default class Square extends requirel('./5-square') {
+  charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
-        console.log(char.repeat(this.width));
+      console.log(char.repeat(this.width));
     }
   }
-};
+}
